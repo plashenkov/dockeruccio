c4b05e5245066e66bbd1d4fd6da57648bebb2f9c
Yuri Plashenkov <plashenkov@gmail.com>
2022-03-21 02:23:33 +0300

    Read version from package.json

--- docker/prod/build.sh
+++ docker/prod/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo Please specify image version:
-read version
+export DOCKER_BUILDKIT=1
+export DOCKER_SCAN_SUGGEST=false
 
 cd ../..
 npx vite build
+version=$(node -p "require('./package.json').version")
 docker build -t plashenkov/dockeruccio:$version docker/prod
